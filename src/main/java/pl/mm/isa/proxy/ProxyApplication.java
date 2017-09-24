36557810652206c55cce62a3f1fc2ac3432e4faf
Mateusz Marzęcki <mateusz.marzecki@grupawp.pl>
2017-09-24 15:51:50 +0200

    init

---
+++ src/main/java/pl/mm/isa/proxy/ProxyApplication.java
@@ -0,0 +1,12 @@
+package pl.mm.isa.proxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProxyApplication.class, args);
+	}
+}
